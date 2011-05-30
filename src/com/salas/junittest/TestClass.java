9dcbb678dc9cb69932aa583a7d2afd895bf96018
Pito Salas <pitosalas@rpsair.(none)>
2011-05-30 10:51:04 -0400

    Dummy always returns 50!

--- src/com/salas/junittest/TestClass.java
+++ src/com/salas/junittest/TestClass.java
@@ -1,10 +1,10 @@
 /**
- * 
+ * A class to do multiplication without using the multiply operation
  */
 package com.salas.junittest;
 
 public class TestClass {
 	public int multiply(int x, int y) {
-		return x * y;
+		return 50;
 	}
 }
